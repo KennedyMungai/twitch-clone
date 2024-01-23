a36edbcaffff03720690fc943ceaa3e01b5fdf5e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-23 03:47:23 +0300

    Set up the Theme Provider for the application

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { dark } from '@clerk/themes'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import ThemeProvider from '@/providers/ThemeProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,9 +19,11 @@ export default function RootLayout({
 }>) {
 	return (
 		<ClerkProvider appearance={{ baseTheme: dark }}>
-			<html lang='en'>
-				<body className={inter.className}>{children}</body>
-			</html>
+			<ThemeProvider>
+				<html lang='en'>
+					<body className={inter.className}>{children}</body>
+				</html>
+			</ThemeProvider>
 		</ClerkProvider>
 	)
 }
