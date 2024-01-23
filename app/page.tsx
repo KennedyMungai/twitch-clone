016f058bf6fac7c00336ff6b45ab2a32053db72a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-23 03:11:20 +0300

    Implemented the logout logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import { UserButton } from '@clerk/nextjs'
+
 export default function Home() {
-	return <main className='min-h-screen'></main>
+	return (
+		<main className='min-h-screen'>
+			<div className='flex flex-col gap-y-4'>
+				<h1>Dashboard</h1>
+				<UserButton afterSignOutUrl='/' />
+			</div>
+		</main>
+	)
 }
