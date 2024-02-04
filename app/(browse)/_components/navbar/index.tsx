3fbf259445cce648bf686760bf94525a37349497
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-04 13:46:14 +0300

    Added some styles for our navbar

--- app/(browse)/_components/navbar/index.tsx
+++ app/(browse)/_components/navbar/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 type Props = {}
 
 const NavBar = (props: Props) => {
-	return <nav>NavBar</nav>
+	return <nav className='fixed top-0 w-full h-20 z-[49] bg-[#252731] px-2 lg:px-4 flex justify-between items-center shadow-sm'>NavBar</nav>
 }
 
 export default NavBar
