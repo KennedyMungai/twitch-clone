de2e1a69582ceac7345b3a59f257ad8780f6be3d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-04 15:26:49 +0300

    Added some markup for the actions component

--- app/(browse)/_components/navbar/actions.tsx
+++ app/(browse)/_components/navbar/actions.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import { Button } from '@/components/ui/button'
+import { SignInButton, currentUser } from '@clerk/nextjs'
+import { Clapperboard } from 'lucide-react'
+import Link from 'next/link'
 
-type Props = {}
+const Actions = async () => {
+	const user = await currentUser()
 
-const Actions = (props: Props) => {
-	return <div>Actions</div>
+	return (
+		<div className='flex items-center justify-end gap-x-2 ml-4 lg:ml-0'>
+			{!user && (
+				<SignInButton>
+					<Button>Login</Button>
+				</SignInButton>
+			)}
+			{!!user && (
+				<div className='flex items-center gap-x-4'>
+					<Button
+						size='sm'
+						variant={'ghost'}
+						className='text-muted-foreground hover:text-primary'
+					>
+						<Link
+							href={`/u/${user.username}`}
+							className='flex flex-col items-center justify-center'
+						>
+							<Clapperboard />
+							<span>Dashboard</span>
+						</Link>
+					</Button>
+				</div>
+			)}
+		</div>
+	)
 }
 
 export default Actions
