712b78376d621e59bbad2d49c806053cca2819f4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-04 13:57:42 +0300

    Added a reference to the Logo component

--- app/(browse)/_components/navbar/index.tsx
+++ app/(browse)/_components/navbar/index.tsx
@@ -1,9 +1,12 @@
+import Logo from '@/app/(auth)/_components/logo'
 import React from 'react'
 
 type Props = {}
 
 const NavBar = (props: Props) => {
-	return <nav className='fixed top-0 w-full h-20 z-[49] bg-[#252731] px-2 lg:px-4 flex justify-between items-center shadow-sm'>NavBar</nav>
+	return <nav className='fixed top-0 w-full h-20 z-[49] bg-[#252731] px-2 lg:px-4 flex justify-between items-center shadow-sm'>
+        <Logo />
+    </nav>
 }
 
 export default NavBar
