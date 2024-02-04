01dbbdea776d35acc743373af910f562f2576653
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-04 12:43:00 +0300

    Deleted and readjusted the console log to not use the ifd value

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -41,10 +41,10 @@ export const POST = async (req: Request) => {
         })
     }
 
-    const { id } = evt.data
+    // const { id } = evt.data
     const eventType = evt.type
 
-    console.log(`Webhook with an ID of ${id} and type of ${eventType}`)
+    console.log(`Webhook with a type of ${eventType}`)
     // console.log('Webhook body: ', body)
 
     return new Response('', { status: 200 })
