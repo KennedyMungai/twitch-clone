d680e4f5aa52dab0147cc8cbc21657f30b776c5a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-04 15:11:51 +0300

    Created the actions component

--- app/(browse)/_components/navbar/index.tsx
+++ app/(browse)/_components/navbar/index.tsx
@@ -1,3 +1,4 @@
+import Actions from "./actions"
 import Logo from "./logo"
 import Search from "./search"
 
@@ -7,6 +8,7 @@ const NavBar = (props: Props) => {
 	return <nav className='fixed top-0 w-full h-20 z-[49] bg-[#252731] px-2 lg:px-4 flex justify-between items-center shadow-sm'>
         <Logo />
         <Search />
+        <Actions />
     </nav>
 }
 
