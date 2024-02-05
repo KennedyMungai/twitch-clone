f16c43c8a6bd16c0b2f28c0d35abf0f642980ede
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-06 02:48:25 +0300

    Added some functionality to the button

--- app/(browse)/_components/sidebar/toggle.tsx
+++ app/(browse)/_components/sidebar/toggle.tsx
@@ -14,7 +14,11 @@ const Toggle = () => {
 			{!collapsed && (
 				<div className='p-3 pl-6 mb-2 flex items-center w-full'>
 					<p className='font-semibold text-primary'>For You</p>
-					<Button className='h-auto p-2 ml-auto' variant={'ghost'}>
+					<Button
+						className='h-auto p-2 ml-auto'
+						variant={'ghost'}
+						onClick={onCollapse}
+					>
 						<ArrowLeftFromLine className='h-4 w-4' />
 					</Button>
 				</div>
