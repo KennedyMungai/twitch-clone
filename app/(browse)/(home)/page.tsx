94f0ececd89d4e16d2739e831c6c945037bb4fb6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-05 19:51:14 +0300

    Changed the text from dashboard to Home page

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
 	return (
 		<main className='min-h-screen'>
 			<div className='flex flex-col gap-y-4'>
-				<h1>Dashboard</h1>
+				<h1>Home Page</h1>
 			</div>
 		</main>
 	)
