c53d6942ce4149809df2e646508e0fef58b8190e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-05 20:01:01 +0300

    Made some updates to the style of the logo

--- app/(browse)/_components/navbar/logo.tsx
+++ app/(browse)/_components/navbar/logo.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 const Logo = () => {
 	return (
 		<Link href='/'>
-			<div className='hidden lg:flex items-center gap-x-4 hover:opacity-75 transition duration-300 ease-out'>
+			<div className='flex items-center gap-x-4 hover:opacity-75 transition duration-300 ease-out'>
 				<div className='bg-transparent rounded-full p-1'>
 					<Image
 						src='/logos/twitch_clone_logo.png'
@@ -13,7 +13,7 @@ const Logo = () => {
 						height={60}
 					/>
 				</div>
-				<div>
+				<div className='hidden lg:flex flex-col'>
 					<p className='text-lg font-semibold'>Twitch Clone</p>
 					<p className='text-xs text-muted-foreground'>
 						Let&apos;s Play
