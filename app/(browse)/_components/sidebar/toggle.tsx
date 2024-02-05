3f4419be18e2f97239d23766bded4b22202e9ce3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-06 02:44:10 +0300

    Added some proper logic to the component

--- app/(browse)/_components/sidebar/toggle.tsx
+++ app/(browse)/_components/sidebar/toggle.tsx
@@ -1,7 +1,21 @@
-type Props = {}
+'use client'
 
-const Toggle = (props: Props) => {
-	return <div>Toggle</div>
+import { useSideBar } from '@/store/use-sidebar'
+
+const Toggle = () => {
+	const { collapsed, onCollapse, onExpand } = useSideBar((state) => state)
+
+	const label = collapsed ? 'Expand' : 'Collapse'
+
+	return (
+		<>
+			{!collapsed && (
+				<div className='p-3 pl-6 mb-2 flex items-center w-full'>
+					<p>For You</p>
+				</div>
+			)}
+		</>
+	)
 }
 
 export default Toggle
