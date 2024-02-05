4b2bbdc02705eeeb155271206455a95c49861585
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-05 20:25:04 +0300

    Gave the sidebar a z-index of 50

--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const Wrapper = ({ children }: Props) => {
 	return (
-		<aside className='fixed top-20 left-0 flex flex-col w-60 h-full bg-background border-r border-[#2d2e35]'>
+		<aside className='fixed top-20 left-0 flex flex-col w-60 h-full bg-background border-r border-[#2d2e35] z-50'>
 			{children}
 		</aside>
 	)
