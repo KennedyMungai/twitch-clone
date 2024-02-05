6e13efdf1bac3dab8010d1801c3d90227be13ffd
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-06 02:33:20 +0300

    Added the useSideba hook and made the component a client component

--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import { useSideBar } from '@/store/use-sidebar'
 import { ReactNode } from 'react'
 
 type Props = {
@@ -5,6 +8,8 @@ type Props = {
 }
 
 const Wrapper = ({ children }: Props) => {
+	const { collapsed } = useSideBar((state) => state)
+
 	return (
 		<aside className='fixed top-20 left-0 flex flex-col w-60 h-full bg-background border-r border-[#2d2e35] z-50'>
 			{children}
