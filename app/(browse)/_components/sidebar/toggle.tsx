9fa37513ae4dafc1e02b7523dc49743571dcfdc1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-06 02:47:15 +0300

    Added the collapse button

--- app/(browse)/_components/sidebar/toggle.tsx
+++ app/(browse)/_components/sidebar/toggle.tsx
@@ -1,6 +1,8 @@
 'use client'
 
+import { Button } from '@/components/ui/button'
 import { useSideBar } from '@/store/use-sidebar'
+import { ArrowLeftFromLine } from 'lucide-react'
 
 const Toggle = () => {
 	const { collapsed, onCollapse, onExpand } = useSideBar((state) => state)
@@ -12,6 +14,9 @@ const Toggle = () => {
 			{!collapsed && (
 				<div className='p-3 pl-6 mb-2 flex items-center w-full'>
 					<p className='font-semibold text-primary'>For You</p>
+					<Button className='h-auto p-2 ml-auto' variant={'ghost'}>
+						<ArrowLeftFromLine className='h-4 w-4' />
+					</Button>
 				</div>
 			)}
 		</>
