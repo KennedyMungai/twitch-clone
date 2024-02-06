15be61cf024d864d9053c639259f14285aa2e1bc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-07 02:57:35 +0300

    Getting the user id

--- lib/recommended-service.ts
+++ lib/recommended-service.ts
@@ -1,6 +1,16 @@
+import { getSelf } from './auth-service'
 import { db } from './db'
 
 export const getRecommended = async () => {
+	let userId
+
+	try {
+		const self = await getSelf()
+		userId = self.id
+	} catch (error) {
+		userId = null
+	}
+
 	const users = await db.user.findMany({
 		orderBy: {
 			createdAt: 'desc'
