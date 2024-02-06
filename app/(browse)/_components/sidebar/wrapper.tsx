c8510d322d2ddc9a1d7da9f580b97334f950c57f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-07 02:41:02 +0300

    Fixed the flickering sidebar issue

--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -14,7 +14,7 @@ const Wrapper = ({ children }: Props) => {
 	return (
 		<aside
 			className={cn(
-				'fixed top-20 left-0 flex flex-col w-60 h-full bg-background border-r border-[#2d2e35] z-50',
+				'fixed top-20 left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2d2e35] z-50',
 				collapsed && 'w-[70px]'
 			)}
 		>
