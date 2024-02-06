6672418be23fc441d745e23c8e918b0d01a9e7cc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-06 04:00:36 +0300

    Added some styles for the container component

--- app/(browse)/_components/container.tsx
+++ app/(browse)/_components/container.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { cn } from '@/lib/utils'
+import { useSideBar } from '@/store/use-sidebar'
 import { ReactNode } from 'react'
 
 type Props = {
@@ -7,7 +9,18 @@ type Props = {
 }
 
 const Container = ({ children }: Props) => {
-	return <div>{children}</div>
+	const { collapsed } = useSideBar((state) => state)
+
+	return (
+		<div
+			className={cn(
+				'flex-1',
+				collapsed ? 'ml-[70px]' : 'ml-[70px] lg:ml-60'
+			)}
+		>
+			{children}
+		</div>
+	)
 }
 
 export default Container
