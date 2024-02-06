13ec9854e2c3fc9453dc4851b99284a66bc7cb37
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-06 03:11:11 +0300

    Used the hint component for showing tooltips on the button

--- app/(browse)/_components/sidebar/toggle.tsx
+++ app/(browse)/_components/sidebar/toggle.tsx
@@ -1,12 +1,7 @@
 'use client'
 
+import Hint from '@/components/hint'
 import { Button } from '@/components/ui/button'
-import {
-    Tooltip,
-    TooltipContent,
-    TooltipProvider,
-    TooltipTrigger
-} from '@/components/ui/tooltip'
 import { useSideBar } from '@/store/use-sidebar'
 import { ArrowLeftFromLine, ArrowRightFromLine } from 'lucide-react'
 
@@ -19,41 +14,31 @@ const Toggle = () => {
 		<>
 			{collapsed && (
 				<div className='hidden lg:flex w-full items-center justify-center pt-4 mb-4'>
-					<TooltipProvider>
-						<Tooltip>
-							<TooltipTrigger asChild>
-								<Button
-									variant={'ghost'}
-									onClick={onExpand}
-									className='h-auto p-2'
-									size='icon'
-								>
-									<ArrowRightFromLine />
-								</Button>
-							</TooltipTrigger>
-							<TooltipContent>{label}</TooltipContent>
-						</Tooltip>
-					</TooltipProvider>
+					<Hint label={label} asChild>
+						<Button
+							variant={'ghost'}
+							onClick={onExpand}
+							className='h-auto p-2'
+							size='icon'
+						>
+							<ArrowRightFromLine />
+						</Button>
+					</Hint>
 				</div>
 			)}
 			{!collapsed && (
 				<div className='p-3 pl-6 mb-2 flex items-center w-full'>
 					<p className='font-semibold text-primary'>For You</p>
-					<TooltipProvider>
-						<Tooltip>
-							<TooltipTrigger asChild>
-								<Button
-									className='h-auto p-2 ml-auto'
-									variant={'ghost'}
-									onClick={onCollapse}
-                                    size={'icon'}
-								>
-									<ArrowLeftFromLine className='h-4 w-4' />
-								</Button>
-							</TooltipTrigger>
-							<TooltipContent>{label}</TooltipContent>
-						</Tooltip>
-					</TooltipProvider>
+					<Hint label={label} asChild>
+						<Button
+							className='h-auto p-2 ml-auto'
+							variant={'ghost'}
+							onClick={onCollapse}
+							size={'icon'}
+						>
+							<ArrowLeftFromLine className='h-4 w-4' />
+						</Button>
+					</Hint>
 				</div>
 			)}
 		</>
