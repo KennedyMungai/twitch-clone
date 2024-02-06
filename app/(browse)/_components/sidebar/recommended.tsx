82e399005cc5e3fb1dad9461d0bb1e3c5d7dfd24
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-06 04:36:10 +0300

    Added some optional rendering for the recommended values in the sidebat

--- app/(browse)/_components/sidebar/recommended.tsx
+++ app/(browse)/_components/sidebar/recommended.tsx
@@ -1,7 +1,24 @@
-type Props = {}
+'use client'
+import { useSideBar } from '@/store/use-sidebar'
+import { User } from '@prisma/client'
 
-const Recommended = (props: Props) => {
-	return <div>Recommended</div>
+type Props = {
+	data: User[]
+}
+
+const Recommended = ({ data }: Props) => {
+	const { collapsed } = useSideBar((state) => state)
+	const showLabel = !collapsed && data.length > 0
+
+	return (
+		<div>
+			{showLabel && (
+				<div className='pl-6 mb-4'>
+					<p className='text-sm text-muted-foreground'>Recommended</p>
+				</div>
+			)}
+		</div>
+	)
 }
 
 export default Recommended
