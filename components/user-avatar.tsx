7bf673c13b96138a16f5195d31e3ba8d409d56d5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-07 00:49:34 +0300

    Created the user-avatar component

---
+++ components/user-avatar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {
+	imageUrl: string
+	userName: string
+	isLive?: boolean
+}
+
+const UserAvatar = (props: Props) => {
+	return <div>UserAvatar</div>
+}
+
+export default UserAvatar
