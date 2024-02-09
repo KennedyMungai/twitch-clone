048a72c187e99072704cf9298bb02c79518a200b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-09 03:18:35 +0300

    Created the followUser function

--- lib/follow-service.ts
+++ lib/follow-service.ts
@@ -29,3 +29,39 @@ export const isFollowingUser = async (id: string) => {
 		return false
 	}
 }
+
+export const followUser = async (id: string) => {
+	const self = await getSelf()
+
+	const otherUser = await db.user.findUnique({
+		where: {
+			id
+		}
+	})
+
+	if (!otherUser) throw new Error('User Not Found ')
+
+	if (otherUser.id === self.id) throw new Error('Cannot Follow Yourself')
+
+	const existingFollow = await db.follow.findFirst({
+		where: {
+			followerId: self.id,
+			followingId: otherUser.id
+		}
+	})
+
+	if (existingFollow) throw new Error('Already following')
+
+	const follow = await db.follow.create({
+		data: {
+			followerId: self.id,
+			followingId: otherUser.id
+		},
+		include: {
+			following: true,
+			follower: true
+		}
+	})
+
+	return follow
+}
