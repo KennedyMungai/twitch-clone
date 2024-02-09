8e71abf97776e02bd2aff6f9ebbe79641b937820
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-09 04:04:27 +0300

    Created the Actions button component

---
+++ app/(browse)/[username]/_components/actions.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+
+type Props = {}
+
+const Actions = (props: Props) => {
+	return <Button variant={'primary'}>Follow</Button>
+}
+
+export default Actions
