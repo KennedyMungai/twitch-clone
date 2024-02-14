91ed41c08af6e30555d156d5d7dd16f0045cadda
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-14 04:07:44 +0300

    Basic markup and styling for the navbar component

--- app/(dashboard)/u/[username]/_components/navbar/actions.tsx
+++ app/(dashboard)/u/[username]/_components/navbar/actions.tsx
@@ -1,5 +1,18 @@
+import { Button } from '@/components/ui/button'
+import { LogOut } from 'lucide-react'
+import Link from 'next/link'
+
 const Actions = () => {
-	return <div className='Chicken'></div>
+	return (
+		<div className='flex items-center justify-end gap-x-2'>
+			<Button>
+				<Link href={'/'}>
+					<LogOut />
+					Exit
+				</Link>
+			</Button>
+		</div>
+	)
 }
 
 export default Actions
